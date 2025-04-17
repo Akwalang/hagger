d45ed3f5292590574408c1f5bed6abd47264d44c
Siarhei Kukunin <kukunins@gmail.com>
2025-04-17 21:38:50 +0200

    feat: complete key-value text editor field

--- src/views/components/Form/components/ContentEditable/ContentEditable.tsx
+++ src/views/components/Form/components/ContentEditable/ContentEditable.tsx
@@ -1,5 +1,7 @@
 import { useRef, useEffect, useState } from "react";
 
+import * as Base from '@/views/ui/dropdown-menu';
+
 import { cn } from "@/utils/react";
 
 import { empower } from "./utils/empower.utils";
@@ -18,36 +20,67 @@ interface ContentEditableProps {
 
 export const ContentEditable: React.FC<ContentEditableProps> = (props) => {
   const [value, setValue] = useState(props.value);
+  const [isOpen, setIsOpen] = useState(false);
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
   const field = useRef<HTMLDivElement>(null);
 
   const onChange = (value: string) => {
     props.onChange?.(value.trim());
+
+    const val = value.toLowerCase();
+
+    const list = (props.options || []).filter((option) => {
+      option = option.toLowerCase();
+
+      return option.includes(val) && option !== val;
+    });
+
     setValue(value);
+    setSuggestions(list);
+    setIsOpen(!!value && !!list.length);
   };
 
   const onSelect = (value: string) => {
     field.current!.textContent = value;
+
     setValue(value);
+    setSuggestions([]);
+    setIsOpen(false);
   };
 
   useEffect(() => empower(field.current!), []);
 
   return (
     <div className={cn("relative", props.className)}>
-      <div
-        ref={field}
-        className={cn(
-          props.fieldClassName,
-          "w-full h-full break-all cursor-text",
-          "focus:outline-0 focus:relative focus:border-primary focus:z-1",
-        )}
-        contentEditable={!props.disabled}
-        suppressContentEditableWarning={true}
-        onInput={() => onChange(field.current!.textContent ?? "")}
-      >
-        {props.value}
-      </div>
-      {!!props.options?.length && <Options value={value} options={props.options} onSelect={onSelect} />}
+      <Base.DropdownMenu open={isOpen} onOpenChange={setIsOpen}>
+        <Base.DropdownMenuTrigger asChild>
+          <div className={cn(
+            props.fieldClassName,
+            "absolute top-0 left-0 w-full h-full pointer-events-none",
+            value === '' || value === '<br>' ? "text-muted-foreground" : "text-transparent",
+          )}>
+            {props.placeholder}
+          </div>
+        </Base.DropdownMenuTrigger>
+        <div
+          key="contenteditable"
+          ref={field}
+          className={cn(
+            props.fieldClassName,
+            "w-full h-full break-all cursor-text",
+            "focus:outline-0 focus:relative focus:border-primary focus:z-1",
+          )}
+          data-placeholder={props.placeholder}
+          contentEditable={!props.disabled}
+          suppressContentEditableWarning={true}
+          onInput={() => onChange(field.current!.textContent ?? "")}
+          onFocus={() => onChange(field.current!.textContent ?? "")}
+        >
+          {props.value}
+        </div>
+        {!!suggestions.length && <Options value={value} options={suggestions} onSelect={onSelect} />}
+      </Base.DropdownMenu>
     </div>
   );
 };
@@ -59,23 +92,27 @@ interface OptionsProps {
 }
 
 const Options: React.FC<OptionsProps> = (props) => {
-  const val = props.value.toLowerCase();
-
-  const list = props.options.filter((option) => {
-    option = option.toLowerCase();
-
-    return option.includes(val) && option !== val;
-  });
-
-  if (!list.length) return null;
+  const preventAutoFocus = {
+    onCloseAutoFocus: (e: Event) => e.preventDefault(),
+    onOpenAutoFocus: (e: Event) => e.preventDefault(),
+  };
 
   return (
-    <div className="absolute w-[160px] top-full left-0">
-      {list.map((option, i) => (
-        <div key={i} className="w-full flex items-center justify-between" onClick={() => props.onSelect(option)}>
-          {option}
-        </div>
-      ))}
-    </div>
+    <Base.DropdownMenuPortal>
+      <Base.DropdownMenuContent
+        className="w-[var(--radix-dropdown-menu-trigger-width)]"
+        {...preventAutoFocus}
+      >
+        {props.options.map((option) => (
+          <Base.DropdownMenuItem
+            key={option}
+            className="cursor-pointer"
+            onClick={() => props.onSelect(option)}
+          >
+            {option}
+          </Base.DropdownMenuItem>
+        ))}
+      </Base.DropdownMenuContent>
+    </Base.DropdownMenuPortal>
   );
-}
+};
