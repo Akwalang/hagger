b2e769cf649f2039f46cf40afb245a63f9825c63
Siarhei Kukunin <kukunins@gmail.com>
2025-04-30 03:03:45 +0200

    feat: disable page context menu

--- src/views/application/App/App.tsx
+++ src/views/application/App/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { AppRouter } from '@/views/application/Router/Router';
 import { Theme } from '@/views/application/Theme/Theme';
 
@@ -5,7 +7,19 @@ import './App.css';
 
 interface AppProps {}
 
+const preventContextMenu = (event: MouseEvent) => {
+  // event.preventDefault();
+};
+
 export const App: React.FC<AppProps> = () => {
+  useEffect(() => {
+    window.addEventListener('contextmenu', preventContextMenu);
+
+    return () => {
+      window.removeEventListener('contextmenu', preventContextMenu);
+    }
+  }, []);
+
   return (
     <>
       <Theme />
